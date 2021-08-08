0e432ccdda653ab7b71b5bedc40e792348ec96e7
Rafael Goesmann <rgo51@uclive.ac.nz>
2021-08-08 14:59:52 +1200

    Added changes

--- components/PersonalProjects.tsx
+++ components/PersonalProjects.tsx
@@ -15,19 +15,19 @@ import codeQuizImage5 from '../public/images/code-quiz/code-quiz-image-1.png'
 
 
 const projects: Project[] = [{
-  name: 'Logicraft - 1st Place Hackathon Project',
-  description: ['The concept of the hackathon was to improve engagement in computer science amoungst kids. We decided to create a project which converts an electronic schematic (a logical representation of an electric circuit) into an equivalent circuit in Minecraft.', 'Minecraft has a powerful concept called Redstone which can be used to emulate electrical circuits. What better way of teaching kids the inner workings of a computer through building interactive circuits in one of the worlds most popular game!'],
+  name: 'Logicraft - 1st Place Hackathon Project at UC Computer Society',
+  description: ['The goal of the hackathon was to improve engagement in computer science amongst children. We decided to create a project which converts an electronic schematic (a logical representation of an electric circuit) into an equivalent circuit in Minecraft.', 'Minecraft has a powerful concept called Redstone which can be used to emulate electrical circuitry. What better way of teaching kids the inner workings of a computer through building interactive circuits in one of the worlds most popular game!'],
   images: [logicraftImage1, logicraftImage2, logicraftImage3]
 
 },
 {
   name: 'Code Quiz',
-  description: ['Created a web app with a co-student that allows teachers to create programming quizzes for their students.', 'When we were high school students, we would have appreciated a more structured way to learn programming and focus more on problem solving instead of just syntax.', 'An interesting aspect was utilising Docker to run untrusted students code in a sandbox. This project was built with Python (Flask), Vue.js, MySQL and Docker.'],
+  description: ['Created a web app with a co-student that allows teachers to create programming quizzes for their students.', 'When I was a high school student, I would have appreciated a more structured way to learn how to program by focusing more on problem solving instead of just learning syntax.', 'An interesting aspect was utilising Docker to run untrusted code in a sandbox. This project was built with Python (Flask), Vue.js, MySQL and Docker.'],
   images: [codeQuizImage1, codeQuizImage2, codeQuizImage3, codeQuizImage4, codeQuizImage5]
 },
 {
   name: 'Automated Game Highlights Bot',
-  description: ['Automated Highlights Bot is a bot which scrapes the site twitch.tv for the most popular clips for that current day. Since Juke Highlights is built with Golang, I was able to use Golang\'s strong concurrency primitives to download the videos concurrently.', 'The program first downloads the most popular videos for that current day, it will then remove duplicates, encode and concatentate them with ffmpeg and then will automaticaly get uploaded to youtube with an automated thumbnail.', 'I wanted my brother to be able to use juke highlights as well for the days that I couldn\'t so I also created an easy to use web interface for the previous command line application.'],
+  description: ['I created a bot which automatically creates a youtube video from the top Twitch.tv clips for a specific game every day.', 'I wanted my brother to be able to use the bot as well for the days that I couldn\'t so I also created an easy to use web interface.', 'Since the bot is built with Golang, I was able to use Golang\'s strong concurrency primitives to download the videos concurrently.'],
   images: [automatedHighlightsBotImage1]
 }];
 
--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -12,21 +12,28 @@ import chillPassImage4 from '../public/images/chill-pass/chill-pass-image-4.png'
 import chillPassImage5 from '../public/images/chill-pass/chill-pass-image-4.png';
 
 
-const projects: Project[] = [{
-    name: 'Full Stack Engineer at Verizon Connect',
-    description: ['Working as a full stack developer in the Compliance team. Primarily developer new features/maintaining an app used by Truck drivers to automatically record their hours of service.', 'I am using various different technologies including .Net Core, Typescript, Node, Postgresql and Angular.', 'An interesting project that my team works on is called the Unidentified Driving Service which automatically detects driving done when a driver is not logged into the app.'],
+const projects: Project[] = [
+  {
+    name: "Full Stack Engineer at Verizon Connect",
+    description: [
+      "As a full stack developer in the Compliance Team, I primarily develop new features and maintain an app used by around 25k unique drivers daily to automatically record their trip details under the Hours of Service Regulations.",
+      "A significant feature that my team developed is called Unidentified Driving. This feature automatically generates driving history when a driver fails to log into the app. This feature automated a task that was previously tracked by 20 full-time staff of one of our customers.",
+      "I generally work on backend features using microservice and event driven architectures. Technologies that I have used across Verizon Connect include .Net Core, Typescript, Node, Postgresql, Angular and many more.",
+    ],
   },
   {
-    name: 'Part-Time Developer at EPEX LTD',
-    description: ['Worked part-time to create a peer-to-peer platform for people to buy and sell houses completely online without real estate agents.',  'The project was developed in Typescript Node, React + Postgresql.'],
-    link: 'https://epex.co.nz',
-    images: [epexImage1, epexImage2, epexImage3, epexImage4]
+    name: "Part-Time Developer at EPEX LTD",
+    description: [
+      "As a student I was part of a team of three developers, contracted by two real estate veterans to found and develop a peer-to-peer platform. The site was launched in 2020 and enables people to buy and sell residential properties online without the need for real estate agents."
+    ],
+    link: "https://epex.co.nz",
+    images: [epexImage1, epexImage2, epexImage3, epexImage4],
   },
   {
-    name: 'Part-Time Developer at Chill Studio',
-    description: ['Worked part-time on a web app/native app for ski pass tracking/management software for a client. ', 'Technologies used include Typescript, Node, Vue.js, React Native and Postgresql. This service automated a task that a customer had 20 people employed full-time to manage.'],
+    name: "Part-Time Developer at Chill Studio",
+    description: ["Worked on a web app for ski pass tracking/management software for a client. I was the sole developer to create a digital ski pass that automated the purchase and processing of previously paper-based ski passes.", "I also integrated the client's Shopify point of sale system into the ski pass software so passes were automatically added after a sale was made through Shopify.", "Technologies used include Typescript, Node, Vue.js, React Native, Postgresql and AWS."],
     images: [chillPassImage1, chillPassImage2, chillPassImage3, chillPassImage4, chillPassImage5],
-  }
+  },
 ];
 
 export default function WorkExperience() {
